ec698e51ee110919e609804cabe9e09b30bb4d3e
AI510-02 <kmf191919@gmail.com>
2023-09-01 16:17:15 +0900

    09-01 목록화면에 사용될 결과행을 출력하는 테스트

--- src/test/java/kr/ac/kopo/board2/repository/BoardRepositoryTests.java
+++ src/test/java/kr/ac/kopo/board2/repository/BoardRepositoryTests.java
@@ -1,11 +1,20 @@
 package kr.ac.kopo.board2.repository;
 
+import jakarta.transaction.Transactional;
 import kr.ac.kopo.board2.entity.Board;
 import kr.ac.kopo.board2.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -28,6 +37,41 @@ public class BoardRepositoryTests {
                     .build();
 
             boardRepository.save(board);
+        });
+    }
+    @Transactional
+    @Test
+    public void testRead1(){
+        Optional<Board> result = boardRepository.findById(100L);//100L은 검색할 글번호
+
+        Board board = result.get();
+        System.out.println(board);
+        System.out.println(board.getWriter());
+    }
+
+    @Test
+    public void testReadWithWriter(){
+        Object result = boardRepository.getBoardWithWriter(50L);
+        Object[] arr = (Object[]) result;
+        System.out.println(Arrays.toString(arr));
+    }
+
+    @Test
+    public void testGetBoardWithReply(){
+        List<Object[]> result = boardRepository.getBoardWithReply(5L);
+
+        for (Object[] arr :result){
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    @Test
+    public void testWithReplyCount(){
+        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+        result.get().forEach(row -> {
+            Object[] arr = (Object[]) row;
+            System.out.println(Arrays.toString(arr));
         });
     }
 }
